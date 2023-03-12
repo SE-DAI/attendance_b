e2326a9aa39f1a2261620b34fd550d68466f6451
seidai <boc.yui.xysy@gmail.com>
2023-03-13 02:07:06 +0900

    Finish edit-attendances

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,36 @@ class ApplicationController < ActionController::Base
 
   $days_of_the_week = %w{日 月 火 水 木 金 土}
 
+  # beforeフィルター
+
+# paramsハッシュからユーザーを取得します。
+def set_user
+  @user = User.find(params[:id])
+end
+
+  # ログイン済みのユーザーか確認します。
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインが必要です。"
+      redirect_to login_url
+    end
+  end
+
+  # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+  def correct_user
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # システム管理権限所有かどうか判定します。
+  def admin_user
+    unless current_user.admin?
+      flash[:warning] = "権限がありません。"
+      redirect_to root_url 
+    end
+  end
+
+
   # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
   def set_one_month 
     @first_day = params[:date].nil? ?

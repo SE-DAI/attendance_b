1421217b1dcde7a6b336025cc0e29a8e598abf88
seidai <boc.yui.xysy@gmail.com>
2023-05-22 00:44:58 +0900

    deploy-last-setting

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,9 @@
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads threads_count, threads_count
+threads min_threads_count, max_threads_count
 
 #port        ENV.fetch("PORT") { 3000 }
-bind "unix:///var/www/test_app/tmp/sockets/puma.sock"
+bind "unix:///var/www/attendance_b/tmp/sockets/puma.sock"
 
 environment ENV.fetch("RAILS_ENV") { "development" }
 
